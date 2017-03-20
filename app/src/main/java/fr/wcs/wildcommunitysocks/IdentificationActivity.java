2e60257cb47df1520149d3983aa80e09b5263984
wilder <marionfontaine904@gmail.com>
2017-03-20 15:46:45 +0100

    With Sign in, Sign up, Sign out on Firebase

--- app/src/main/java/fr/wcs/wildcommunitysocks/IdentificationActivity.java
+++ app/src/main/java/fr/wcs/wildcommunitysocks/IdentificationActivity.java
@@ -1,46 +1,108 @@
 package fr.wcs.wildcommunitysocks;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class IdentificationActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class IdentificationActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonLogIn;
+    private TextView textViewSignUp;
+    private EditText editTextUserPwd;
+    private EditText editTextuserEmail;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identification);
 
-        Button identification = (Button) findViewById(R.id.id_confirm);
-        Button cancel_id=(Button) findViewById(R.id.id_cancel);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        identification.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //When an existing user identifies, he arrives on the main stream : Flux
-                Intent intent_identification = new Intent(IdentificationActivity.this,Flux.class);
-                /**
-                 * here will be detailed the connexion checking :
-                 * - at first : if there is a name and a password
-                 * - when connected to a database : if the user exists
-                 */
-                startActivity(intent_identification);
+        //if getCurrentUser does not returns null
+        if (firebaseAuth.getCurrentUser() != null) {
+            //that means user is already logged in
+            //so close this activity
+            finish();
+
+            //and open profile activity
+            startActivity(new Intent(getApplicationContext(), Navigation.class));
+        }
 
 
-            }
-        });
+        buttonLogIn = (Button) findViewById(R.id.buttonLogin);
+        textViewSignUp = (TextView) findViewById(R.id.textViewSignUp);
+        editTextUserPwd =(EditText) findViewById(R.id.editUserPwd);
+        editTextuserEmail=(EditText) findViewById(R.id.editUserEmail);
 
-        cancel_id.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_back = new Intent(IdentificationActivity.this,MainActivity.class);
-                /**
-                 * Possibly a pop-up to ask for confirmation
-                 */
-                startActivity(intent_back);
-            }
-        });
+        progressDialog = new ProgressDialog(this);
+
+        buttonLogIn.setOnClickListener(this);
+        textViewSignUp.setOnClickListener(this);
+    }
+
+    public void userLogin(){
+        //getting email and password from edit texts
+        String email = editTextuserEmail.getText().toString().trim();
+        String password  = editTextUserPwd.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(IdentificationActivity.this,getString(R.string.toastDefaultEmail),Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(IdentificationActivity.this,getString(R.string.toastDefaultPwd),Toast.LENGTH_LONG).show();
+            return;
+        }
+        //if the email and password are not empty
+        //displaying a progress dialog
+        progressDialog.setMessage(getString(R.string.signInPending));
+        progressDialog.show();
+
+        //logging in the user
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        //if the task is successfull
+                        if(task.isSuccessful()){
+                            //start the profile activity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), Navigation.class));
+                        }else{
+                            Toast.makeText(getApplicationContext(),getString(R.string.signInFailed),Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==buttonLogIn){
+            userLogin();
+        }
+        if(v==textViewSignUp){
+            startActivity(new Intent(IdentificationActivity.this,NewAccountActivity.class));
+        }
     }
 }
--- app/src/main/java/fr/wcs/wildcommunitysocks/NewAccountActivity.java
+++ app/src/main/java/fr/wcs/wildcommunitysocks/NewAccountActivity.java
@@ -1,50 +1,119 @@
 package fr.wcs.wildcommunitysocks;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class NewAccountActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class NewAccountActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //defining view objects
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignIn;
+    private Button buttonSignup;
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
 
-        Button button_new_confirm =(Button) findViewById(R.id.button_new_confirm);
-        Button button_new_cancel=(Button) findViewById(R.id.button_new_cancel);
+        //initializing firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        button_new_confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_create= new Intent(NewAccountActivity.this, Navigation.class);
-                /**
-                 * After checking the mandatory information is complete :
-                 * Pseudo
-                 * Mail
-                 * PWD
-                 *
-                 * The new user is then redirected to his newly created Profile page
-                 * where he will be able to complete (if he wishes to)
-                 * Picture
-                 * foot size
-                 * sentence
-                 */
-                startActivity(intent_create);
-            }
-        });
+        //if getCurrentUser does not returns null
+        if (firebaseAuth.getCurrentUser() != null) {
+            //that means user is already logged in
+            //so close this activity
+            finish();
 
-        button_new_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_cancel= new Intent (NewAccountActivity.this, MainActivity.class);
-                /**
-                 * Possibly a pop-up to ask for confirmation
-                 */
-                startActivity(intent_cancel);
-            }
-        });
+            //and open profile activity
+            startActivity(new Intent(getApplicationContext(), Navigation.class));
+        }
+
+        //initializing views
+        buttonSignup = (Button) findViewById(R.id.button_new_confirm);
+        textViewSignIn = (TextView) findViewById(R.id.alreadyRegistered);
+        editTextEmail = (EditText) findViewById(R.id.editEmail);
+        editTextPassword = (EditText) findViewById(R.id.editPwd);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching listener to button
+        buttonSignup.setOnClickListener(this);
+        textViewSignIn.setOnClickListener(this);
+    }
+
+    public void registerUser(){
+        //getting email and password from edit texts
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(NewAccountActivity.this,getString(R.string.toastDefaultEmail),Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(NewAccountActivity.this,getString(R.string.toastDefaultPwd),Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        progressDialog.setMessage(getString(R.string.registrationPending));
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), Navigation.class));
+                        }else{
+                            //display some message here
+                           // Log.e(TAG, "Sign-in Failed: " + task.getException().getMessage());
+                            // If the connection keeps failing un comment this :
+                            //Toast.makeText(NewAccountActivity.this,"Sign-in Failed: " + task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                            Toast.makeText(NewAccountActivity.this,getString(R.string.registrationFailed),Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v==buttonSignup){
+            registerUser();
+        }
+        if(v==textViewSignIn){
+            startActivity(new Intent(NewAccountActivity.this,IdentificationActivity.class));
+        }
     }
 }
