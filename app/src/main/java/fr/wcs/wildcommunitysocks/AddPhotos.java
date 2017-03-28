8b4f80897283ab6a0bdb7d5bb0672a410016dfa8
wilder <marionfontaine904@gmail.com>
2017-03-28 11:02:46 +0200

    Picture taking _ without scaling

--- app/src/main/java/fr/wcs/wildcommunitysocks/AddPhotos.java
+++ app/src/main/java/fr/wcs/wildcommunitysocks/AddPhotos.java
@@ -1,11 +1,13 @@
 package fr.wcs.wildcommunitysocks;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
@@ -13,6 +15,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,7 +35,7 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
 
     //private StorageReference mStorageRef;
     private static final int PICK_PHOTO = 100;
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 234;
     private String sdf = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
     ImageView showPhoto;
     private Uri imageUri;
@@ -33,6 +43,7 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
     private Button buttonSelectFromGallery;
     private Button buttonUpload;
     private String mCurrentPhotoPath;
+    private StorageReference mStorageRef;
 
 
     public static AddPhotos newInstance() {
@@ -48,8 +59,7 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-       // mStorageRef= FirebaseStorage.getInstance().getReference();
+        mStorageRef= FirebaseStorage.getInstance().getReference();
         View view = inflater.inflate(R.layout.fragment_add_photos, container, false);
 
         showPhoto = (ImageView) view.findViewById(R.id.imageView);
@@ -63,19 +73,22 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
 
         return view;
     }
+
+
+
     private void openGallery() {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        gallery.setType("image/*");
+         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+         gallery.setType("image/*");
 
-        gallery.putExtra("crop", "true");
-        gallery.putExtra("scale", "true");
-        gallery.putExtra("outputX", 200);
-        gallery.putExtra("outputY", 200);
-        gallery.putExtra("aspectX", 1);
-        gallery.putExtra("aspectY", 1);
-        gallery.putExtra("return-data", "true");
+         gallery.putExtra("crop", "true");
+         gallery.putExtra("scale", true);
+         gallery.putExtra("outputX", 256);
+         gallery.putExtra("outputY", 256);
+         gallery.putExtra("aspectX", 1);
+         gallery.putExtra("aspectY", 1);
+         gallery.putExtra("return-data", true);
 
-        startActivityForResult(gallery, PICK_PHOTO);
+         startActivityForResult(gallery, PICK_PHOTO);
     }
 
     private void dispatchTakePictureIntent() {
@@ -83,6 +96,7 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
         /**Ensure there is a camera activity to handle the Intent*/
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
             /** Create the file where the photo should go
              */
             File photoFile=null;
@@ -93,8 +107,7 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(getActivity(),
-                        "com.example.android.fileprovider", photoFile);
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),"com.example.android.fileprovider", photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
 
             }
@@ -105,13 +118,9 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        /**NB modified from 'protected' to 'public' to avoid conflict**/
         if (resultCode == RESULT_OK && requestCode == PICK_PHOTO) {
-            Bundle extras = data.getExtras();
             imageUri = data.getData();
             showPhoto.setImageURI(imageUri);
-            //Bitmap imageBitmap = (Bitmap) extras.get("data");
-            //showPhoto.setImageBitmap(imageBitmap);
             //galleryAddPic();
             return;
         }
@@ -121,13 +130,13 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
             imageUri=data.getData();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             showPhoto.setImageBitmap(imageBitmap);
-
+            //photo.setImageURI(imageUri);
             galleryAddPic();
             return;
         }
     }
     private File createImageFile() throws IOException {
-        // Create an image file name
+       // Create an image file name
         String sdf = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + sdf + "_";
         File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
@@ -150,22 +159,56 @@ public class AddPhotos extends Fragment implements View.OnClickListener{
     }
 
     private void uploadFile(){
+        if(imageUri!=null) {
+            final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("Chargement en cours...");
+            progressDialog.show();
+            StorageReference picRef = mStorageRef.child("images/"+sdf+".jpg");
 
+            picRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), "Upload successfull", Toast.LENGTH_LONG);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), exception.getMessage(), Toast.LENGTH_LONG);
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>(){
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage("Uploaded :"+(int)progress+"%");
+
+                        }
+
+
+                    });
+        }
+        else{
+            Toast.makeText(getActivity(),"Foirage", Toast.LENGTH_SHORT);
+        }
     }
 
     @Override
     public void onClick(View v) {
-        if(v==buttonTakePicture){
+        if (v == buttonTakePicture) {
             dispatchTakePictureIntent();
         }
-        if(v==buttonSelectFromGallery){
+        if (v == buttonSelectFromGallery) {
             openGallery();
         }
-        if(v==buttonUpload){
+        if (v == buttonUpload) {
             uploadFile();
         }
 
     }
-
 
 }
