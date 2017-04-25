6b18cc605541287e5ccf5c52c15d73ce4ece2698
lebiam <antoine.biamouret@gmail.com>
2017-04-25 16:15:56 +0200

    change placeholder

--- app/src/main/java/fr/wcs/wildcommunitysocks/Credits.java
+++ app/src/main/java/fr/wcs/wildcommunitysocks/Credits.java
@@ -1,13 +1,30 @@
 package fr.wcs.wildcommunitysocks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Credits extends AppCompatActivity {
+public class Credits extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton imageButtonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        imageButtonBack = (ImageButton) findViewById(R.id.imageButtonBack);
+        imageButtonBack.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v==imageButtonBack){
+            finish();
+        }
+    }
+
 }
